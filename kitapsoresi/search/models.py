a09c239e82b087e78dafb02ed618c10cb2786e54
S-nega <103982287+S-nega@users.noreply.github.com>
2023-02-26 16:56:53 +0600

    Delete kitapsoresi directory

--- kitapsoresi/search/models.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 2720c0d5935e0ce6009ef6ec6937aa87da4356c7
